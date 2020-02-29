9aa5a64a59f4b45d000761517db24ee4650cd78f
Kadriye <kadriyemacit9655@gmail.com>
2020-02-29 17:52:08 +0300

    anasayfa ui bitti

--- lib/another_pages/home_page.dart
+++ lib/another_pages/home_page.dart
@@ -63,7 +63,7 @@ class HomeLayout extends State<HomePage> with TickerProviderStateMixin {
                   left: 20.0,
                   child: IconButton(
                     onPressed: () => print('Add to My List'),
-                    icon: Icon(Icons.add),
+                    icon: Icon(Icons.favorite_border),
                     iconSize: 40.0,
                     color: Colors.black,
                   ),
@@ -73,7 +73,7 @@ class HomeLayout extends State<HomePage> with TickerProviderStateMixin {
                   right: 25.0,
                   child: IconButton(
                     onPressed: () => print('Share'),
-                    icon: Icon(Icons.share),
+                    icon: Image.asset("assets/out.png"),
                     iconSize: 35.0,
                     color: Colors.black,
                   ),
@@ -82,14 +82,190 @@ class HomeLayout extends State<HomePage> with TickerProviderStateMixin {
               ],
           ),
 
+          Text("Welcome!"),
+
+          Padding(
+            padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
+            child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+
+                Padding(
+                  padding: const EdgeInsets.only(top: 10, bottom: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: <Widget>[
+
+                      Container(
+                          width: 90,
+                          height: 70,
+                          decoration: BoxDecoration(
+                            color: Color.fromRGBO(93, 142, 155, 1.0),
+                              borderRadius: new BorderRadius.only(
+                                  topLeft:  const  Radius.circular(10.0),
+                                  topRight: const  Radius.circular(10.0),
+                                  bottomLeft: const  Radius.circular(10.0),
+                                  bottomRight: const  Radius.circular(10.0),)
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                  width: 50,
+                                  height: 50,
+                                  child: Image.asset("assets/menu/notes.png")
+                              ),
+                              Text("take a not!")
+                            ],
+                          ),
+                        ),
+
+
+                      Container(
+                          width: 90,
+                          height: 70,
+                          decoration: BoxDecoration(
+                              color: Color.fromRGBO(93, 142, 155, 1.0),
+                              borderRadius: new BorderRadius.only(
+                                topLeft:  const  Radius.circular(10.0),
+                                topRight: const  Radius.circular(10.0),
+                                bottomLeft: const  Radius.circular(10.0),
+                                bottomRight: const  Radius.circular(10.0),)
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                  width: 50,
+                                  height: 50,
+                                  child: Image.asset("assets/menu/food.png")
+                              ),
+                              Text("healthy")
+                            ],
+                          ),
+                        ),
+
+
+                    Container(
+                          width: 90,
+                          height: 70,
+                          decoration: BoxDecoration(
+                              color: Color.fromRGBO(93, 142, 155, 1.0),
+                              borderRadius: new BorderRadius.only(
+                                topLeft:  const  Radius.circular(10.0),
+                                topRight: const  Radius.circular(10.0),
+                                bottomLeft: const  Radius.circular(10.0),
+                                bottomRight: const  Radius.circular(10.0),)
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                  width: 50,
+                                  height: 50,
+                                  child: Image.asset("assets/menu/game.png")
+                              ),
+                              Text("gaming")
+                            ],
+                          ),
+                        ),
+
+
+                    ],
+                  ),
+                ),
+
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+
+                     Container(
+                        width: 90,
+                        height: 70,
+                        decoration: BoxDecoration(
+                            color: Color.fromRGBO(93, 142, 155, 1.0),
+                            borderRadius: new BorderRadius.only(
+                              topLeft:  const  Radius.circular(10.0),
+                              topRight: const  Radius.circular(10.0),
+                              bottomLeft: const  Radius.circular(10.0),
+                              bottomRight: const  Radius.circular(10.0),)
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                                width: 50,
+                                height: 50,
+                                child: Image.asset("assets/menu/travel.png")
+                            ),
+                            Text("travel")
+                          ],
+                        ),
+                      ),
+
+
+                     Container(
+                        width: 90,
+                        height: 70,
+                        decoration: BoxDecoration(
+                            color: Color.fromRGBO(93, 142, 155, 1.0),
+                            borderRadius: new BorderRadius.only(
+                              topLeft:  const  Radius.circular(10.0),
+                              topRight: const  Radius.circular(10.0),
+                              bottomLeft: const  Radius.circular(10.0),
+                              bottomRight: const  Radius.circular(10.0),)
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Container(
+                                width: 50,
+                                height: 50,
+                                child: Image.asset("assets/menu/translate.png")
+                            ),
+                            Text("work hard!")
+                          ],
+                        ),
+                      ),
+
+
+                  Container(
+                        width: 90,
+                        height: 70,
+                        decoration: BoxDecoration(
+                            color: Color.fromRGBO(93, 142, 155, 1.0),
+                            borderRadius: new BorderRadius.only(
+                              topLeft:  const  Radius.circular(10.0),
+                              topRight: const  Radius.circular(10.0),
+                              bottomLeft: const  Radius.circular(10.0),
+                              bottomRight: const  Radius.circular(10.0),)
+                        ),
+                        child: Center(
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                  width: 50,
+                                  height: 50,
+                                  child: Image.asset("assets/menu/life.png")
+                              ),
+                              Text("happy life")
+                            ],
+                          ),
+                        ),
+                      ),
+
+
+                  ],
+                )
+              ],
+            ),
+          )
+
+            ],
+          )
 
 
 
+      );
 
-        ],
-      )
 
-    );
   }
 }
 
